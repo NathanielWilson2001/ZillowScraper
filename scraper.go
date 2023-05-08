428b5137b7a40866c772adaee9de337c61063387
Nathaniel Wilson <nt680476@dal.ca>
2023-05-08 11:59:59 -0400

    altered formating for output

--- scraper.go
+++ scraper.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -38,9 +39,61 @@ type Response struct {
 	} `json:"cat1"`
 }
 
+func responseToString(responseStructure Response) {
+
+	b, err := json.MarshalIndent(responseStructure, "", "\t")
+	_ = err
+
+	fmt.Printf(string(b))
+	fmt.Println()
+	return
+}
+
+func calculate(responseStructure Response) {
+
+	averagePriceSum := 0.0
+	averageSquareFootSum := 0.0
+	averageZestimate := 0.0
+	averageZRentEstimate := 0.0
+	var housingType [3]int
+
+	for i := 0; i < len(responseStructure.Cat1.SearchResults.ListResults); i++ {
+
+		averagePriceSum += float64(responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo.Price)
+		averageSquareFootSum += float64(responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo.LivingArea)
+		averageZRentEstimate += float64(responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo.RentZestimate)
+		averageZestimate += float64(responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo.Zestimate)
+
+		if strings.Contains(responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo.HomeType, "MULTI") {
+			housingType[0]++
+		}
+		if strings.Contains(responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo.HomeType, "SINGLE") {
+			housingType[1]++
+		}
+		if strings.Contains(responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo.HomeType, "CONDO") {
+			housingType[2]++
+		}
+
+	}
+	averagePriceSum = float64(averagePriceSum / float64(len(responseStructure.Cat1.SearchResults.ListResults)))
+	averageSquareFootSum = float64(averageSquareFootSum / float64(len(responseStructure.Cat1.SearchResults.ListResults)))
+	averageZRentEstimate = float64(averageZRentEstimate / float64(len(responseStructure.Cat1.SearchResults.ListResults)))
+	averageZestimate = float64(averageZestimate / float64(len(responseStructure.Cat1.SearchResults.ListResults)))
+
+	fmt.Printf("Count: %d \nAverage Price = %.2f \nAverage Square Foot = %.2f\nAverage Price per Square Foot = %.2f\nAverage Zestimate = %.2f\nAverage ZRentEstimate = %.2f\nHousing Type Breakdown:\n\tMultifamily: %d\n\tSingle Family: %d\n\tCondo: %d", len(responseStructure.Cat1.SearchResults.ListResults),
+		averagePriceSum,
+		averageSquareFootSum,
+		averagePriceSum/averageSquareFootSum,
+		averageZestimate,
+		averageZestimate,
+		housingType[0],
+		housingType[1],
+		housingType[2])
+}
+
 func main() {
 
-	url := "https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=%7B%22mapBounds%22%3A%7B%22north%22%3A43.1847127353123%2C%22south%22%3A41.512239125025815%2C%22east%22%3A-70.65074389648439%2C%22west%22%3A-71.68071215820314%7D%2C%22mapZoom%22%3A9%2C%22isMapVisible%22%3Afalse%2C%22filterState%22%3A%7B%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22sortSelection%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%7D%2C%22isListVisible%22%3Atrue%7D&wants={%22cat1%22:[%22listResults%22],%22cat2%22:[%22total%22],%22regionResults%22:[%22regionResults%22]}&requestId=2"
+	url := "https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=%7B%22mapBounds%22%3A%7B%22north%22%3A42.28936961396935%2C%22east%22%3A-70.90983246728517%2C%22south%22%3A41.86942883946931%2C%22west%22%3A-71.16732453271486%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22isAllHomes%22%3A%7B%22value%22%3Atrue%7D%2C%22sortSelection%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22isRecentlySold%22%3A%7B%22value%22%3Atrue%7D%2C%22isForSaleByAgent%22%3A%7B%22value%22%3Afalse%7D%2C%22isForSaleByOwner%22%3A%7B%22value%22%3Afalse%7D%2C%22isNewConstruction%22%3A%7B%22value%22%3Afalse%7D%2C%22isComingSoon%22%3A%7B%22value%22%3Afalse%7D%2C%22isAuction%22%3A%7B%22value%22%3Afalse%7D%2C%22isForSaleForeclosure%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%2C%22regionSelection%22%3A%5B%7B%22regionId%22%3A58710%2C%22regionType%22%3A7%7D%5D%2C%22mapZoom%22%3A11%2C%22pagination%22%3A%7B%7D%7D&wants=%7B%22cat1%22:[%22listResults%22,%22mapResults%22]%7D&requestId=3"
 	client := http.Client{Timeout: time.Second * 5}
 
 	request, err := http.NewRequest(http.MethodGet, url, nil)
@@ -74,12 +127,13 @@ func main() {
 
 	body, err := io.ReadAll(reader)
 
-	jsonResponse, err := json.Marshal(string(body))
-	_ = jsonResponse
 	responseStructure := &Response{}
 	json.Unmarshal(body, &responseStructure)
-	fmt.Println(responseStructure)
+	//fmt.Println(responseStructure)
+	responseToString(*responseStructure)
 	if err != nil {
 		panic(err)
 	}
+
+	calculate(*responseStructure)
 }
