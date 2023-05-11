7a27670f1f93cdcef940d2aa235f4488f4ac9753
Nathaniel Wilson <nt680476@dal.ca>
2023-05-11 10:39:37 -0400

    Made makeRequest loop through n pages and store as same dataset

--- scraper.go
+++ scraper.go
@@ -59,7 +59,7 @@ func responseToString(responseStructure Response) {
 *	Gets the Average Price, Average Sq Footage, Average Zestimate, Average Rent Zestimate
 *	Outputs them to the console as a String
  */
-func calculate(responseStructure Response) {
+func calculate(responses []Response, numberOfPages int) {
 
 	// Initilize Base values to 0
 	averagePriceSum := 0.0
@@ -67,36 +67,43 @@ func calculate(responseStructure Response) {
 	averageZestimate := 0.0
 	averageZRentEstimate := 0.0
 	var housingType [3]int
+	runningTotalEntries := 0
+	for pageNumber := 0; pageNumber < numberOfPages; pageNumber++ {
 
-	numOfEntries := len(responseStructure.Cat1.SearchResults.ListResults)
-	// Loop through all entries in result
-	for i := 0; i < numOfEntries; i++ {
+		responseStructure := responses[pageNumber]
+		numOfEntries := len(responseStructure.Cat1.SearchResults.ListResults)
 
-		currentEntry := responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo
+		runningTotalEntries += numOfEntries
+		// Loop through all entries in result
+		for i := 0; i < numOfEntries; i++ {
 
-		averagePriceSum += float64(currentEntry.Price)
-		averageSquareFootSum += float64(currentEntry.LivingArea)
-		averageZRentEstimate += float64(currentEntry.RentZestimate)
-		averageZestimate += float64(currentEntry.Zestimate)
+			currentEntry := responseStructure.Cat1.SearchResults.ListResults[i].HdpData.HomeInfo
 
-		if strings.Contains(currentEntry.HomeType, "MULTI") {
-			housingType[0]++
+			averagePriceSum += float64(currentEntry.Price)
+			averageSquareFootSum += float64(currentEntry.LivingArea)
+			averageZRentEstimate += float64(currentEntry.RentZestimate)
+			averageZestimate += float64(currentEntry.Zestimate)
+
+			if strings.Contains(currentEntry.HomeType, "MULTI") {
+				housingType[0]++
+			}
+			if strings.Contains(currentEntry.HomeType, "SINGLE") {
+				housingType[1]++
+			}
+			if strings.Contains(currentEntry.HomeType, "CONDO") {
+				housingType[2]++
+			}
+
 		}
-		if strings.Contains(currentEntry.HomeType, "SINGLE") {
-			housingType[1]++
-		}
-		if strings.Contains(currentEntry.HomeType, "CONDO") {
-			housingType[2]++
-		}
-
 	}
 	// Calculate the average
-	averagePriceSum = float64(averagePriceSum / float64(numOfEntries))
-	averageSquareFootSum = float64(averageSquareFootSum / float64(numOfEntries))
-	averageZRentEstimate = float64(averageZRentEstimate / float64(numOfEntries))
-	averageZestimate = float64(averageZestimate / float64(numOfEntries))
+	averagePriceSum = float64(averagePriceSum / float64(runningTotalEntries))
+	averageSquareFootSum = float64(averageSquareFootSum / float64(runningTotalEntries))
+	averageZRentEstimate = float64(averageZRentEstimate / float64(runningTotalEntries))
+	averageZestimate = float64(averageZestimate / float64(runningTotalEntries))
 
-	fmt.Printf("Count: %d \nAverage Price = %.2f \nAverage Square Foot = %.2f\nAverage Price per Square Foot = %.2f\nAverage Zestimate = %.2f\nAverage ZRentEstimate = %.2f\nHousing Type Breakdown:\n\tMultifamily: %d\n\tSingle Family: %d\n\tCondo: %d\n", len(responseStructure.Cat1.SearchResults.ListResults),
+	fmt.Printf("Count: %d \nAverage Price = %.2f \nAverage Square Foot = %.2f\nAverage Price per Square Foot = %.2f\nAverage Zestimate = %.2f\nAverage ZRentEstimate = %.2f\nHousing Type Breakdown:\n\tMultifamily: %d\n\tSingle Family: %d\n\tCondo: %d\n",
+		runningTotalEntries,
 		averagePriceSum,
 		averageSquareFootSum,
 		averagePriceSum/averageSquareFootSum,
@@ -114,118 +121,123 @@ func calculate(responseStructure Response) {
 *	in the response object. Response calculate is called to output the calculations from the given data set.
  */
 func makeRequest(north float64, south float64, east float64, west float64, numPages int) {
-	url := "https://www.zillow.com/search/GetSearchPageState.htm?"
-	client := http.Client{Timeout: time.Second * 5}
 
-	request, err := http.NewRequest(http.MethodGet, url, nil)
-	query := request.URL.Query()
+	var responses []Response
+	for pageNumber := 1; pageNumber <= numPages; pageNumber++ {
+		url := "https://www.zillow.com/search/GetSearchPageState.htm?"
+		client := http.Client{Timeout: time.Second * 5}
 
-	// URL parameter struct created to form the 'searchQueryState' data necessary for the request.
-	type urlParamters struct {
-		MapBounds struct {
-			North float64 `json:"north"`
-			South float64 `json:"south"`
-			East  float64 `json:"east"`
-			West  float64 `json:"west"`
-		} `json:"mapBounds"`
-		MapZoom      int  `json:"mapZoom"`
-		IsMapVisible bool `json:"isMapVisible"`
-		FilterState  struct {
-			IsAllHomes struct {
-				Value bool `json:"value"`
-			} `json:"isAllHomes"`
-			SortSelection struct {
-				Value string `json:"value"`
-			} `json:"sortSelection"`
-		} `json:"filterState"`
-		IsListVisible bool `json:"isListVisible"`
-		Pagination    struct {
-			CurrentPage int `json:"currentPage"`
-		} `json:"pagination"`
+		request, err := http.NewRequest(http.MethodGet, url, nil)
+		query := request.URL.Query()
+
+		// URL parameter struct created to form the 'searchQueryState' data necessary for the request.
+		type urlParamters struct {
+			MapBounds struct {
+				North float64 `json:"north"`
+				South float64 `json:"south"`
+				East  float64 `json:"east"`
+				West  float64 `json:"west"`
+			} `json:"mapBounds"`
+			MapZoom      int  `json:"mapZoom"`
+			IsMapVisible bool `json:"isMapVisible"`
+			FilterState  struct {
+				IsAllHomes struct {
+					Value bool `json:"value"`
+				} `json:"isAllHomes"`
+				SortSelection struct {
+					Value string `json:"value"`
+				} `json:"sortSelection"`
+			} `json:"filterState"`
+			IsListVisible bool `json:"isListVisible"`
+			Pagination    struct {
+				CurrentPage int `json:"currentPage"`
+			} `json:"pagination"`
+		}
+
+		// URL 'wants' parameter that attaches to the end of the request link.
+		type urlWants struct {
+			Cat1          [1]string `json:"cat1"`
+			Cat2          [1]string `json:"cat2"`
+			RegionResults [1]string `json:"regionResults"`
+		}
+
+		params := &urlParamters{}
+
+		// Set values for URL Parameters
+		params.MapBounds.North = north
+		params.MapBounds.South = south
+		params.MapBounds.East = east
+		params.MapBounds.West = west
+		params.MapZoom = 9
+		params.IsMapVisible = false
+		params.FilterState.IsAllHomes.Value = true
+		params.FilterState.SortSelection.Value = "globalrelevanceex"
+		params.IsListVisible = true
+		params.Pagination.CurrentPage = pageNumber
+
+		// Set want values
+		wants := &urlWants{}
+		wants.Cat1[0] = "listResults"
+		wants.Cat2[0] = "total"
+		wants.RegionResults[0] = "regionResults"
+
+		// Attach structs to the query and encode
+		b, _ := json.Marshal(params)
+		query.Add("searchQueryState", string(b))
+
+		b, _ = json.Marshal(wants)
+		query.Add("wants", string(b))
+
+		request.URL.RawQuery = query.Encode()
+
+		// Set default header values and encoding type
+		request.Header.Set("User-Agent", "User")
+		request.Header.Set("Accept", "*/")
+		request.Header.Set("Accept-Encoding", "gzip")
+		request.Header.Set("Accept-Language", "en-US,en;q=0.9")
+		request.Header.Set("cache-control", "no-cache")
+		request.Header.Set("pragma", "no-cache")
+		request.Header.Set("sec-fetch-dest", "empty")
+		request.Header.Set("sec-fetch-mode", "cors")
+		request.Header.Set("sec-ch-ua", "'Google Chrome';v='113', 'Chromium';v='113', 'Not-A.Brand';v='24'")
+		request.Header.Set("sec-fetch-site", "same-origin")
+		request.Header.Set("sec-ch-ua-mobile", "?0")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Perform request and decode response
+		response, resErr := client.Do(request)
+		if resErr != nil {
+			log.Fatal(err)
+		}
+
+		reader, err := gzip.NewReader(response.Body)
+		if err != nil {
+			panic(err)
+		}
+		defer reader.Close()
+
+		// Store response into a Response struct
+		body, err := io.ReadAll(reader)
+		responseStructure := &Response{}
+		json.Unmarshal(body, &responseStructure)
+
+		//responseToString(*responseStructure)
+		if err != nil {
+			panic(err)
+		}
+
+		responses = append(responses, *responseStructure)
 	}
 
-	// URL 'wants' parameter that attaches to the end of the request link.
-	type urlWants struct {
-		Cat1          [1]string `json:"cat1"`
-		Cat2          [1]string `json:"cat2"`
-		RegionResults [1]string `json:"regionResults"`
-	}
+	calculate(responses, numPages)
 
-	params := &urlParamters{}
-
-	// Set values for URL Parameters
-	params.MapBounds.North = north
-	params.MapBounds.South = south
-	params.MapBounds.East = east
-	params.MapBounds.West = west
-	params.MapZoom = 9
-	params.IsMapVisible = false
-	params.FilterState.IsAllHomes.Value = true
-	params.FilterState.SortSelection.Value = "globalrelevanceex"
-	params.IsListVisible = true
-	params.Pagination.CurrentPage = numPages
-
-	// Set want values
-	wants := &urlWants{}
-	wants.Cat1[0] = "listResults"
-	wants.Cat2[0] = "total"
-	wants.RegionResults[0] = "regionResults"
-
-	// Attach structs to the query and encode
-	b, _ := json.Marshal(params)
-	query.Add("searchQueryState", string(b))
-
-	b, _ = json.Marshal(wants)
-	query.Add("wants", string(b))
-
-	request.URL.RawQuery = query.Encode()
-
-	// Set default header values and encoding type
-	request.Header.Set("User-Agent", "User")
-	request.Header.Set("Accept", "*/")
-	request.Header.Set("Accept-Encoding", "gzip")
-	request.Header.Set("Accept-Language", "en-US,en;q=0.9")
-	request.Header.Set("cache-control", "no-cache")
-	request.Header.Set("pragma", "no-cache")
-	request.Header.Set("sec-fetch-dest", "empty")
-	request.Header.Set("sec-fetch-mode", "cors")
-	request.Header.Set("sec-ch-ua", "'Google Chrome';v='113', 'Chromium';v='113', 'Not-A.Brand';v='24'")
-	request.Header.Set("sec-fetch-site", "same-origin")
-	request.Header.Set("sec-ch-ua-mobile", "?0")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Perform request and decode response
-	response, resErr := client.Do(request)
-	if resErr != nil {
-		log.Fatal(err)
-	}
-
-	reader, err := gzip.NewReader(response.Body)
-	if err != nil {
-		panic(err)
-	}
-	defer reader.Close()
-
-	// Store response into a Response struct
-	body, err := io.ReadAll(reader)
-	responseStructure := &Response{}
-	json.Unmarshal(body, &responseStructure)
-
-	//responseToString(*responseStructure)
-	if err != nil {
-		panic(err)
-	}
-
-	calculate(*responseStructure)
 }
 func main() {
 
-	makeRequest(42.28936961396935, 41.86942883946931, -70.90983246728517, -71.16732453271486, 1)
-	makeRequest(42.28936961396935, 41.86942883946931, -70.90983246728517, -71.16732453271486, 2)
-	makeRequest(42.28936961396935, 41.86942883946931, -70.90983246728517, -71.16732453271486, 3)
+	makeRequest(42.28936961396935, 41.86942883946931, -70.90983246728517, -71.16732453271486, 10)
 
 	//makeRequest(43.1847127353123, 41.512239125025815, -70.65074389648439, -71.68071215820314, 1)
 
