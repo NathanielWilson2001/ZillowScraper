169524eb9f3b02ac2db48406bd7ad03582034d80
Nathaniel Wilson <nt680476@dal.ca>
2023-06-08 22:50:46 -0400

    json format changes, css changes

--- scraper.go
+++ scraper.go
@@ -63,55 +63,62 @@ type Calculations struct {
 	Condo                     int     `json:"condo"`
 	Date                      string  `json:"timeOfCompletion"`
 }
+
 type Data struct {
-	Boston struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Boston"`
-	Brooklyn struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Brooklyn"`
-	LosAngeles struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"LosAngeles"`
-	Dallas struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Dallas"`
-	Philadelphia struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Philadelphia"`
-	WashingtonDC struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"WashingtonDC"`
-	Baltimore struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Baltimore"`
-	Jacksonville struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Jacksonville"`
-	Charlotte struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Charlotte"`
-	Nashville struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Nashville"`
-	Memphis struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Memphis"`
-	SanFrancisco struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"SanFrancisco"`
-	Seattle struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Seattle"`
-	Portland struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Portland"`
-	SanDiego struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"SanDiego"`
-	Anchorage struct {
-		Calculations []Calculations `json:"data"`
-	} `json:"Anchorage"`
+	NorthEast struct {
+		Boston struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Boston"`
+		Brooklyn struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Brooklyn"`
+		Philadelphia struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Philadelphia"`
+		WashingtonDC struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"WashingtonDC"`
+		Baltimore struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Baltimore"`
+	} `json:"NorthEast"`
+	South struct {
+		Dallas struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Dallas"`
+		Jacksonville struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Jacksonville"`
+		Charlotte struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Charlotte"`
+		Nashville struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Nashville"`
+		Memphis struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Memphis"`
+	} `json:"South"`
+	WestCoast struct {
+		LosAngeles struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"LosAngeles"`
+		SanFrancisco struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"SanFrancisco"`
+		Seattle struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Seattle"`
+		Portland struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Portland"`
+		SanDiego struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"SanDiego"`
+		Anchorage struct {
+			Calculations []Calculations `json:"data"`
+		} `json:"Anchorage"`
+	} `json:"WestCoast"`
 }
 
 /*
@@ -191,6 +198,15 @@ func calculate(responses []Response, numberOfPages int) Calculations {
 		Date:                      currentTime,
 	}
 	return calculations
+}
+
+/*
+*
+*
+*
+ */
+func calculateRegion(w http.ResponseWriter, r *http.Request) {
+
 }
 
 /*
@@ -317,7 +333,6 @@ func makeRequest(north float64, south float64, east float64, west float64, numPa
 }
 
 /* Send Fetch Data Request */
-
 func fetchData(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
 	jsonFile, _ := os.Open("src/dataStorage.json")
@@ -331,72 +346,71 @@ func fetchData(w http.ResponseWriter, r *http.Request) {
 	switch string(body) {
 	case "NorthEast":
 		listItem.Location = "Boston"
-		listItem.Calculations = dataStructure.Boston.Calculations[len(dataStructure.Boston.Calculations)-1]
+		listItem.Calculations = dataStructure.NorthEast.Boston.Calculations[len(dataStructure.NorthEast.Boston.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Brooklyn"
-		listItem.Calculations = dataStructure.Brooklyn.Calculations[len(dataStructure.Brooklyn.Calculations)-1]
+		listItem.Calculations = dataStructure.NorthEast.Brooklyn.Calculations[len(dataStructure.NorthEast.Brooklyn.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Philadelphia"
-		listItem.Calculations = dataStructure.Philadelphia.Calculations[len(dataStructure.Philadelphia.Calculations)-1]
+		listItem.Calculations = dataStructure.NorthEast.Philadelphia.Calculations[len(dataStructure.NorthEast.Philadelphia.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Washington DC"
-		listItem.Calculations = dataStructure.WashingtonDC.Calculations[len(dataStructure.WashingtonDC.Calculations)-1]
+		listItem.Calculations = dataStructure.NorthEast.WashingtonDC.Calculations[len(dataStructure.NorthEast.WashingtonDC.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Baltimore"
-		listItem.Calculations = dataStructure.Baltimore.Calculations[len(dataStructure.Baltimore.Calculations)-1]
+		listItem.Calculations = dataStructure.NorthEast.Baltimore.Calculations[len(dataStructure.NorthEast.Baltimore.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 	case "South":
 
 		listItem.Location = "Dallas"
-		listItem.Calculations = dataStructure.Dallas.Calculations[len(dataStructure.Dallas.Calculations)-1]
+		listItem.Calculations = dataStructure.South.Dallas.Calculations[len(dataStructure.South.Dallas.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Jacksonville"
-		listItem.Calculations = dataStructure.Jacksonville.Calculations[len(dataStructure.Jacksonville.Calculations)-1]
+		listItem.Calculations = dataStructure.South.Jacksonville.Calculations[len(dataStructure.South.Jacksonville.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Charlotte"
-		listItem.Calculations = dataStructure.Charlotte.Calculations[len(dataStructure.Charlotte.Calculations)-1]
+		listItem.Calculations = dataStructure.South.Charlotte.Calculations[len(dataStructure.South.Charlotte.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Nashville"
-		listItem.Calculations = dataStructure.Nashville.Calculations[len(dataStructure.Nashville.Calculations)-1]
+		listItem.Calculations = dataStructure.South.Nashville.Calculations[len(dataStructure.South.Nashville.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Memphis"
-		listItem.Calculations = dataStructure.Memphis.Calculations[len(dataStructure.Memphis.Calculations)-1]
+		listItem.Calculations = dataStructure.South.Memphis.Calculations[len(dataStructure.South.Memphis.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 	case "WestCoast":
 		listItem.Location = "Los Angeles"
-		listItem.Calculations = dataStructure.LosAngeles.Calculations[len(dataStructure.LosAngeles.Calculations)-1]
+		listItem.Calculations = dataStructure.WestCoast.LosAngeles.Calculations[len(dataStructure.WestCoast.LosAngeles.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "San Francisco"
-		listItem.Calculations = dataStructure.SanFrancisco.Calculations[len(dataStructure.SanFrancisco.Calculations)-1]
+		listItem.Calculations = dataStructure.WestCoast.SanFrancisco.Calculations[len(dataStructure.WestCoast.SanFrancisco.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Seattle"
-		listItem.Calculations = dataStructure.Seattle.Calculations[len(dataStructure.Seattle.Calculations)-1]
+		listItem.Calculations = dataStructure.WestCoast.Seattle.Calculations[len(dataStructure.WestCoast.Seattle.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Portland"
-		listItem.Calculations = dataStructure.Portland.Calculations[len(dataStructure.Portland.Calculations)-1]
+		listItem.Calculations = dataStructure.WestCoast.Portland.Calculations[len(dataStructure.WestCoast.Portland.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "San Diego"
-		listItem.Calculations = dataStructure.SanDiego.Calculations[len(dataStructure.SanDiego.Calculations)-1]
+		listItem.Calculations = dataStructure.WestCoast.SanDiego.Calculations[len(dataStructure.WestCoast.SanDiego.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
 
 		listItem.Location = "Anchorage"
-		listItem.Calculations = dataStructure.Anchorage.Calculations[len(dataStructure.Anchorage.Calculations)-1]
+		listItem.Calculations = dataStructure.WestCoast.Anchorage.Calculations[len(dataStructure.WestCoast.Anchorage.Calculations)-1]
 		responseToWeb.CityDataList = append(responseToWeb.CityDataList, *listItem)
-
 	}
 
 	toSend, _ := json.Marshal(responseToWeb)
@@ -410,29 +424,28 @@ func fill() {
 	json.Unmarshal(jsonData, &dataStructure)
 	fmt.Println("Fill Start")
 	/* North East */
-	dataStructure.Boston.Calculations = append(dataStructure.Boston.Calculations, makeRequest(42.414140424834216, 42.21256130186242, -70.8934730834961, -71.20177691650392, 10))
-	dataStructure.Brooklyn.Calculations = append(dataStructure.Brooklyn.Calculations, makeRequest(40.758364183254, 40.551558686334644, -73.73687118896483, -74.13855881103514, 10))
-	dataStructure.Philadelphia.Calculations = append(dataStructure.Philadelphia.Calculations, makeRequest(40.202093630241286, 39.78440980977288, -74.81724916699217, -75.4338568330078, 10))
-	dataStructure.WashingtonDC.Calculations = append(dataStructure.WashingtonDC.Calculations, makeRequest(38.995548, 38.791645, -76.909393, -77.119759, 10))
-	dataStructure.Baltimore.Calculations = append(dataStructure.Baltimore.Calculations, makeRequest(39.396749, 39.197207, -76.529453, -76.711519, 10))
+	dataStructure.NorthEast.Boston.Calculations = append(dataStructure.NorthEast.Boston.Calculations, makeRequest(42.414140424834216, 42.21256130186242, -70.8934730834961, -71.20177691650392, 10))
+	dataStructure.NorthEast.Brooklyn.Calculations = append(dataStructure.NorthEast.Brooklyn.Calculations, makeRequest(40.758364183254, 40.551558686334644, -73.73687118896483, -74.13855881103514, 10))
+	dataStructure.NorthEast.Philadelphia.Calculations = append(dataStructure.NorthEast.Philadelphia.Calculations, makeRequest(40.202093630241286, 39.78440980977288, -74.81724916699217, -75.4338568330078, 10))
+	dataStructure.NorthEast.WashingtonDC.Calculations = append(dataStructure.NorthEast.WashingtonDC.Calculations, makeRequest(38.995548, 38.791645, -76.909393, -77.119759, 10))
+	dataStructure.NorthEast.Baltimore.Calculations = append(dataStructure.NorthEast.Baltimore.Calculations, makeRequest(39.396749, 39.197207, -76.529453, -76.711519, 10))
 
 	/* South */
-	dataStructure.Dallas.Calculations = append(dataStructure.Dallas.Calculations, makeRequest(33.04672230257906, 32.588541447873666, -96.3757438779297, -97.17911912207032, 10))
-	dataStructure.Jacksonville.Calculations = append(dataStructure.Jacksonville.Calculations, makeRequest(30.586232, 30.098988, -81.328404, -82.049502, 10))
-	dataStructure.Charlotte.Calculations = append(dataStructure.Charlotte.Calculations, makeRequest(35.431860761322845, 34.98640854430645, -80.42966737792969, -81.23304262207031, 10))
-	dataStructure.Nashville.Calculations = append(dataStructure.Nashville.Calculations, makeRequest(36.405496, 35.989226, -86.515588, -87.054903, 10))
-	dataStructure.Memphis.Calculations = append(dataStructure.Memphis.Calculations, makeRequest(35.264187, 34.994185, -89.637081, -90.304493, 10))
+	dataStructure.South.Dallas.Calculations = append(dataStructure.South.Dallas.Calculations, makeRequest(33.04672230257906, 32.588541447873666, -96.3757438779297, -97.17911912207032, 10))
+	dataStructure.South.Jacksonville.Calculations = append(dataStructure.South.Jacksonville.Calculations, makeRequest(30.586232, 30.098988, -81.328404, -82.049502, 10))
+	dataStructure.South.Charlotte.Calculations = append(dataStructure.South.Charlotte.Calculations, makeRequest(35.431860761322845, 34.98640854430645, -80.42966737792969, -81.23304262207031, 10))
+	dataStructure.South.Nashville.Calculations = append(dataStructure.South.Nashville.Calculations, makeRequest(36.405496, 35.989226, -86.515588, -87.054903, 10))
+	dataStructure.South.Memphis.Calculations = append(dataStructure.South.Memphis.Calculations, makeRequest(35.264187, 34.994185, -89.637081, -90.304493, 10))
 
 	/* MidWest */
 
 	/* West Coast */
-
-	dataStructure.LosAngeles.Calculations = append(dataStructure.LosAngeles.Calculations, makeRequest(34.4717411923813, 33.567993762094694, -117.60835725585937, -119.21510774414062, 10))
-	dataStructure.SanFrancisco.Calculations = append(dataStructure.SanFrancisco.Calculations, makeRequest(37.842914, 37.707608, -122.32992, -122.536739, 10))
-	dataStructure.Seattle.Calculations = append(dataStructure.Seattle.Calculations, makeRequest(47.734145, 47.491912, -122.224433, -122.465159, 10))
-	dataStructure.Portland.Calculations = append(dataStructure.Portland.Calculations, makeRequest(45.714497, 45.395871, -122.471849, -122.919539, 10))
-	dataStructure.SanDiego.Calculations = append(dataStructure.SanDiego.Calculations, makeRequest(33.114249, 32.534175, -116.90816, -117.309797, 10))
-	dataStructure.Anchorage.Calculations = append(dataStructure.Anchorage.Calculations, makeRequest(61.326922, 60.733791, -148.473475, -150.420615, 10))
+	dataStructure.WestCoast.LosAngeles.Calculations = append(dataStructure.WestCoast.LosAngeles.Calculations, makeRequest(34.4717411923813, 33.567993762094694, -117.60835725585937, -119.21510774414062, 10))
+	dataStructure.WestCoast.SanFrancisco.Calculations = append(dataStructure.WestCoast.SanFrancisco.Calculations, makeRequest(37.842914, 37.707608, -122.32992, -122.536739, 10))
+	dataStructure.WestCoast.Seattle.Calculations = append(dataStructure.WestCoast.Seattle.Calculations, makeRequest(47.734145, 47.491912, -122.224433, -122.465159, 10))
+	dataStructure.WestCoast.Portland.Calculations = append(dataStructure.WestCoast.Portland.Calculations, makeRequest(45.714497, 45.395871, -122.471849, -122.919539, 10))
+	dataStructure.WestCoast.SanDiego.Calculations = append(dataStructure.WestCoast.SanDiego.Calculations, makeRequest(33.114249, 32.534175, -116.90816, -117.309797, 10))
+	dataStructure.WestCoast.Anchorage.Calculations = append(dataStructure.WestCoast.Anchorage.Calculations, makeRequest(61.326922, 60.733791, -148.473475, -150.420615, 10))
 
 	updated, _ := json.Marshal((dataStructure))
 	_ = ioutil.WriteFile("src/dataStorage.json", updated, 0777)
--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,5 @@
 const urlParams = new URLSearchParams(window.location.search);
-const locationName = urlParams.get('location');   
+locationName = urlParams.get('location');   
 var cityList;
  fetch('/fetchData', {
   method: 'POST',
@@ -17,7 +17,14 @@ var cityList;
 })
 .then(data => {
   console.log(data.cityDataList[0].location)
-  cityList = data.cityDataList;
+  cityList = data.cityDataList
+  switch(locationName){
+    case "NorthEast":
+        locationName = "North East"
+    case "WestCoast":
+        locationName = "West Coast"   
+  }
+
   document.getElementById("body").innerHTML += 
   `<div class="bg-white mb-48 p-12 text-center text-neutral-700 top-0">
   <h2 class="mb-4 text-4xl font-semibold">`+locationName+` United States</h2>
@@ -33,9 +40,11 @@ var cityList;
     type="button"
     data-te-ripple-init
     data-te-ripple-color="light"
-    class="rounded bg-cyan-400 text-white px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal  shadow-[0_4px_9px_-4px_#0891b2] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] dark:shadow-[0_4px_9px_-4px_#0891b2] dark:hover:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] dark:focus:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] dark:active:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2]">
+    class="disc
+    ">
     Regional Trends
   </button>
+  <div id="trends"></div>
 </div>`;
   var count = 0;
   data.cityDataList.forEach(element => {
@@ -45,34 +54,34 @@ var cityList;
     <div class="flex font-sans shadow-2xl w-7/12 h-10/12">
       <div class="flex-none w-1/3 relative" id="image"><img src="images/`+element.location+`.jpg" alt="`+element.location+`" class="absolute inset-0 w-full h-full object-cover rounded-l-lg" loading="lazy" /></div>
           <form class="flex-auto p-6  bg-white" name="dataValues`+count+`">
-            <div class="flex flex-wrap">
-              <h1 class="flex-auto font-medium text-2xl text-slate-900" id="Count">`+ element.data.runningTotalEntries + ` Houses Anaylized!</h1>
-              <div class="w-full flex-none mt-2 order-1 text-3xl font-bold text-cyan-600" id="Title">Typical House in `+element.location+`</div>
-            </div>
-            <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
-              <div class="space-x-2 flex text-sm font-bold">
-                <label>
-                  <input class="sr-only peer" name="size" type="radio" onclick="changeOutput('averagePrice', '`+count+`')" checked/>
-                  <div class="w-14 h-14 rounded-full flex items-center justify-center text-cyan-400 peer-checked:bg-cyan-600 peer-checked:text-white"><span class="text-2xl">&#36;</span></div>
-                </label>
-                <label>
-                  <input class="sr-only peer" name="size" type="radio" value="s"  onclick="changeOutput('averageSquareFootage', '`+count+`')"/>
-                  <div class="w-14 h-14 rounded-full flex items-center justify-center text-cyan-400 peer-checked:bg-cyan-600 peer-checked:text-white"><span class="text-2xl">ft<sup>2</sup></span></div>
-                </label>
-                <label>
-                  <input class="sr-only peer" name="size" type="radio"  onclick="changeOutput('Zillow', '`+count+`')"/>
-                  <div class="w-14 h-14 rounded-full flex items-center justify-center text-cyan-400 peer-checked:bg-cyan-600 peer-checked:text-white"><span class="text-lg font-bold">Zillow</span></div>
-                </label>
-                 <label>
-                  <input class="sr-only peer" name="size" type="radio"  onclick="changeOutput('Trends', '`+count+`')"/>
-                  <div class="w-14 h-14 rounded-full flex items-center justify-center text-cyan-400 peer-checked:bg-cyan-600 peer-checked:text-white"><span class="text-lg font-bold">Trends</span></div>
-                </label>
+                <div class="flex flex-wrap">
+                  <h1 class="flex-auto font-medium text-2xl text-slate-900" id="Count">`+ element.data.runningTotalEntries + ` Houses Anaylized!</h1>
+                  <div class="w-full flex-none mt-2 order-1 text-3xl font-bold text-cyan-600" id="Title">Typical House in `+element.location+`</div>
+                </div>
+                <div class="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200">
+                  <div class="space-x-2 flex text-sm font-bold">
+                    <label>
+                      <input class="sr-only peer" name="size" type="radio" onclick="changeOutput('averagePrice', '`+count+`')" checked/>
+                      <div class="w-16 h-16 rounded-full flex items-center justify-center text-cyan-400 text-2xl peer-checked:text-white peer-checked:bg-cyan-400 peer-checked:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] peer-checked:outline-none peer-checked:ring-0><span class="text-2xl">&#36;</span></div>
+                    </label>
+                    <label>
+                      <input class="sr-only peer" name="size" type="radio" value="s"  onclick="changeOutput('averageSquareFootage', '`+count+`')"/>
+                      <div class="w-16 h-16 rounded-full flex items-center justify-center text-cyan-400 text-2xl peer-checked:text-white peer-checked:bg-cyan-400 peer-checked:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] peer-checked:outline-none peer-checked:ring-0><span class="text-2xl">ft<sup>2</sup></span></div>
+                    </label>
+                    <label>
+                      <input class="sr-only peer" name="size" type="radio"  onclick="changeOutput('Zillow', '`+count+`')"/>
+                      <div class="w-16 h-16 rounded-full flex items-center justify-center text-cyan-400 text-xl peer-checked:text-white peer-checked:bg-cyan-400 peer-checked:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] peer-checked:outline-none peer-checked:ring-0><span class="text-lg font-bold">Zillow</span></div>
+                    </label>
+                    <label>
+                      <input class="sr-only peer" name="size" type="radio"  onclick="changeOutput('Trends', '`+count+`')"/>
+                      <div class="w-16 h-16 rounded-full flex items-center justify-center text-cyan-400 text-xl peer-checked:text-white peer-checked:bg-cyan-400 peer-checked:shadow-[0_8px_9px_-4px_#0891b2,0_4px_18px_0_#0891b2] peer-checked:outline-none peer-checked:ring-0><span class="text-lg font-bold">Type</span></div>
+                    </label>
+                    </div>
+                  </div>
+                  <div class="w-max flex space-x-4 mb-5 text-sm font-medium h-1/2"> <div class="w-max flex-auto flex space-x-4 mr-4 mt-4">
+                  <p class="w-max text-black tracking-wide text-xl antialiased proportional-nums" id="Data`+count+`">Is worth $`+cityList[count].data.averagePriceSum.toFixed(2)+`</p>
                 </div>
               </div>
-              <div class="flex space-x-4 mb-5 text-sm font-medium h-1/2"> <div class="flex-auto flex space-x-4 mt-4">
-              <p class="text-black tracking-wide text-xl antialiased proportional-nums" id="Data`+count+`">Is worth $`+cityList[count].data.averagePriceSum.toFixed(2)+`</p>
-            </div>
-          </div>
             </form>
           </div>
       </div>`;
@@ -87,7 +96,7 @@ var cityList;
 function changeOutput(value, count){
     styleElement = document.getElementById("Data"+count);
     if(value == "averagePrice"){
-    styleElement.innerHTML = "Is worth $" + cityList[count].data.averagePriceSum.toFixed(2)
+    styleElement.innerHTML = `<h4 class="text-xl">Is worth</h4> <p>$` + cityList[count].data.averagePriceSum.toFixed(2)+`</p>`
     }
     if(value == "averageSquareFootage"){
         styleElement.textContent = "Is " + cityList[count].data.averageSquareFootSum.toFixed(2) + " square feet"
@@ -118,7 +127,7 @@ function changeOutput(value, count){
       options: {
         title: {
           display: true,
-          text: "Housing type makeup",
+          text: "Housing Type Makeup",
           fontSize: 24,
           padding:5
         },
